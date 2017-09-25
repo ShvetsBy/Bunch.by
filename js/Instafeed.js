95c99077ad3dbedab9ebfe511f6be94529b5b9fd
Shvets Ivan <iShvets@MacBook-Air-Shvets.local>
2017-09-26 01:12:57 +0300

    headings

---
+++ js/Instafeed.js
@@ -0,0 +1,7 @@
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'букетминск',
+        clientId: '6fa8222544ab4dd096c548144335c3e0'
+    });
+    feed.run();
---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'igminsk',
+        clientId: '6fa8222544ab4dd096c548144335c3e0'
+    });
+    feed.run();
+
